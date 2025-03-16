58d58782e41187d2dcbe62dc8c205d5d05accd57
slyblaze <jonatan.sly@proton.me>
2025-03-16 17:59:25 -0300

    CONTENT_2.1

--- src/contents/tema1.js
+++ src/contents/tema1.js
@@ -2,30 +2,69 @@ const tema1Content = [
     {
       id: "section1",
       title: "El Proyecto de la Instalación Sanitaria",
-      content: `
-        1. El Proyecto debe poder ser ejecutado con los medios tecnológicos disponibles.\n
-        2. Debe ser capaz de soportar las condiciones de uso normales durante su periodo de vida útil.\n
-        3. En ninguna parte del edificio se debe poner en riesgo la seguridad o la salud de sus ocupantes.\n
-        4. Debe proporcionar las condiciones bioambientales adecuadas para el desarrollo de las actividades a cuyo destino responde el proyecto.\n
-        5. Los costos de la construcción, operación mantenimiento y reparación previsto durante la vida útil deben estar dentro de los márgenes impuestos por el comitente.\n
-        6. Debe cumplir con lineamientos estéticos.\n
-        7. El proyecto y la ejecución deben respetar restricciones establecidas por las Normas y los Órganos de Contralor.
-      `,
+      content:
+        <ul>
+          <li>El Proyecto debe poder ser ejecutado con los medios tecnológicos disponibles.</li>
+          <li>Debe ser capaz de soportar las condiciones de uso normales durante su periodo de vida útil.</li>
+          <li>En ninguna parte del edificio se debe poner en riesgo la seguridad o la salud de sus ocupantes.</li>
+          <li>Debe proporcionar las condiciones bioambientales adecuadas para el desarrollo de las actividades a cuyo destino responde el proyecto.</li>
+          <li>Los costos de la construcción, operación mantenimiento y reparación previsto durante la vida útil deben estar dentro de los márgenes impuestos por el comitente.</li>
+          <li>Debe cumplir con lineamientos estéticos.</li>
+          <li>El proyecto y la ejecución deben respetar restricciones establecidas por las Normas y los Órganos de Contralor.</li>
+        </ul>,
       images: ["/assets/img1.jpg", "/assets/img2.jpg"],
       video: "/assets/video1.mp4"
     },
     {
       id: "section2",
-      title: "Detalles Técnicos del Tema 1",
-      content: `
-        Aquí se detallan aspectos técnicos, fórmulas o conceptos clave.
-      `,
+      title: "Obras Externas de Provisión de Agua Potable",
+      content:
+      <div> 
+        <p>
+          La Provisión de Agua Potable involucra una serie de instalaciones
+          y tratamientos que permiten que esta llegue a los edificios con las
+          carcteríasticas exigidas por el Código Alimentario Nacional de cantidad
+          y calidad necesaria para cubrir las demandas conforme al destino de los
+          mismos.
+        </p>
+        <p>
+          En términos generales vamos a decir que el tipo de Captación es 
+          definido por lo que se denomina "Estudio de Identificación y Evaluación
+          de Fuentes de Agua" y las íneas de tratamiento por las caracteríasticas
+          físico químicas y bactereológicas del agua captada.
+        </p>
+        <ul>
+          <li><h4>Captación.</h4>
+            <ul>
+              <li>Dentro de cauce</li>
+              <li>Fuera de cauce</li>
+              <li>Estanques</li>
+            </ul>
+          </li>
+          <li>Cañerías de Aducción</li>
+          <li>Tratamiento</li>
+          <li>Reserva</li>
+        </ul>
+
+        <ul>
+          <li>Dentro de cauce</li>
+          <li>Fuera de cauce</li>
+          <li>Estanques</li>
+        </ul>
+        </div>,
       images: [],
       video: null
     },
     {
       id: "section3",
-      title: "Ejemplos Prácticos del Tema 1",
+      title: "El Ciclo del Agua en el Edificio",
+      content: `La distribución del agua en el edificio se hace en presión, por lo tanto, la forma en que se reseulve la instalación interna del edificio depende de la presión disponible en lared pública de agua potable.`,
+      images: [],
+      video: null
+    },
+    {
+      id: "section4",
+      title: "Reglamentos y Normas",
       content: `Listado de casos prácticos o ejercicios.`,
       images: [],
       video: null
