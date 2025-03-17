5699a23220a152c88e19af59071165b21617e46d
slyblaze <jonatan.sly@proton.me>
2025-03-16 22:38:21 -0300

    CONTENT_v2.2

--- src/contents/tema1.js
+++ src/contents/tema1.js
@@ -3,15 +3,17 @@ const tema1Content = [
       id: "section1",
       title: "El Proyecto de la Instalación Sanitaria",
       content:
-        <ul>
-          <li>El Proyecto debe poder ser ejecutado con los medios tecnológicos disponibles.</li>
-          <li>Debe ser capaz de soportar las condiciones de uso normales durante su periodo de vida útil.</li>
-          <li>En ninguna parte del edificio se debe poner en riesgo la seguridad o la salud de sus ocupantes.</li>
-          <li>Debe proporcionar las condiciones bioambientales adecuadas para el desarrollo de las actividades a cuyo destino responde el proyecto.</li>
-          <li>Los costos de la construcción, operación mantenimiento y reparación previsto durante la vida útil deben estar dentro de los márgenes impuestos por el comitente.</li>
-          <li>Debe cumplir con lineamientos estéticos.</li>
-          <li>El proyecto y la ejecución deben respetar restricciones establecidas por las Normas y los Órganos de Contralor.</li>
-        </ul>,
+        <div>
+          <ol>
+            <li><h4>El Proyecto debe poder ser ejecutado con los medios tecnológicos disponibles.</h4></li>
+            <li><h4>Debe ser capaz de soportar las condiciones de uso normales durante su periodo de vida útil.</h4></li>
+            <li><h4>En ninguna parte del edificio se debe poner en riesgo la seguridad o la salud de sus ocupantes.</h4></li>
+            <li><h4>Debe proporcionar las condiciones bioambientales adecuadas para el desarrollo de las actividades a cuyo destino responde el proyecto.</h4></li>
+            <li><h4>Los costos de la construcción, operación mantenimiento y reparación previsto durante la vida útil deben estar dentro de los márgenes impuestos por el comitente.</h4></li>
+            <li><h4>Debe cumplir con lineamientos estéticos.</h4></li>
+            <li><h4>El proyecto y la ejecución deben respetar restricciones establecidas por las Normas y los Órganos de Contralor.</h4></li>
+          </ol>
+        </div>,
       images: ["/assets/img1.jpg", "/assets/img2.jpg"],
       video: "/assets/video1.mp4"
     },
@@ -33,39 +35,76 @@ const tema1Content = [
           de Fuentes de Agua" y las íneas de tratamiento por las caracteríasticas
           físico químicas y bactereológicas del agua captada.
         </p>
-        <ul>
+        <ol>
           <li><h4>Captación.</h4>
-            <ul>
-              <li>Dentro de cauce</li>
-              <li>Fuera de cauce</li>
-              <li>Estanques</li>
-            </ul>
+              <ul>
+                <li>Superficial: Cursos de Agua
+                  <ul>
+                    <li>Dentro de cauce</li>
+                    <li>Fuera de cauce</li>
+                    <li>Estanques</li>
+                  </ul>
+                </li>
+                <li>Subterráneas:
+                  <ul> 
+                    <li>Pozos Profundos</li>
+                    <li>Pozos Someros</li>
+                  </ul>
+                </li>
+                <li>Subálveos de cursos de agua
+                  <ul> 
+                    <li>Dentro de cauce</li>
+                    <li>Fuera de cauce</li>
+                  </ul>
+                </li>
+                <li>Agua de lluvia</li>
+              </ul>
           </li>
-          <li>Cañerías de Aducción</li>
-          <li>Tratamiento</li>
-          <li>Reserva</li>
-        </ul>
-
-        <ul>
-          <li>Dentro de cauce</li>
-          <li>Fuera de cauce</li>
-          <li>Estanques</li>
-        </ul>
-        </div>,
+          <li><h4>Cañerías de Aducción:</h4>
+            <dd>Conduce el agua desde la captación
+            hasta el tratamiento.</dd>
+          </li>
+          <li><h4>Tratamiento:</h4>
+            <dd>Coagulación, floculación y sedimentación,
+              filtración, desinfección.</dd></li>
+          <li><h4>Reserva:</h4>
+            <dd>Compensación de variaciones de consumos,
+              reserva y mantenimiento de presiones en la
+              red de distribución.</dd>
+          </li>
+        </ol>
+      </div>,
       images: [],
       video: null
     },
     {
       id: "section3",
       title: "El Ciclo del Agua en el Edificio",
-      content: `La distribución del agua en el edificio se hace en presión, por lo tanto, la forma en que se reseulve la instalación interna del edificio depende de la presión disponible en lared pública de agua potable.`,
-      images: [],
+      content: 
+        <p>
+          La distribución del agua en el edificio se hace en
+          presión, por lo tanto, la forma en que se reseulve
+          la instalación interna del edificio depende de la
+          presión disponible en lared pública de agua potable.
+        </p>,
+        images: [],
       video: null
     },
     {
       id: "section4",
       title: "Reglamentos y Normas",
-      content: `Listado de casos prácticos o ejercicios.`,
+      content:
+        <ul>
+          <li>
+            <p>Reglamento de Instalaciones Sanitarias Internas:
+              Anexo Normas OSN - Ciudad de Salta</p>
+          </li>
+          <li>
+            <p>Código de Edificación de la Ciudad de Buenos Aires:
+              Anexo Reglamento sobre Instalaciones Sanitarias</p>
+          </li>
+        </ul>
+      ,
       images: [],
       video: null
     }
