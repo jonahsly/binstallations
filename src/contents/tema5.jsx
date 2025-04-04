9f90acc922ebcb65810faff880d3c6af94a9cc50
jonahsly <jonatan.sly@proton.me>
2025-04-04 23:51:03 +0000

    CONTENT_v3.9.4

--- src/contents/tema5.jsx
+++ src/contents/tema5.jsx
@@ -232,12 +232,189 @@ const tema5Content = [
         carga de estos accesorios en forma individual.
         <h3>Caudales de Cálculo de la Instalación:</h3>
         <h4>Caudal Instalado Q<sub>t</sub></h4>
+        Se define como Caudal Instalado a la sumatoria de los consumos asignados a los artefactos sanitarios,
+        que son alimentados por el tramo que se considere.
+
+
+
         <h4>Caudal Simultáneo Q<sub>c</sub></h4>
+        Se define como Caudal Simultáneo al CAUDAL MÁXIMO PROBABLE que circula por un dado tramo de la
+        instalación, bajo una cierta simultaneidad en el uso de los artefactos que son alimentados por dicho
+        tramo.
         <h3>Simultaneidad de Uso de los Artefactos Sanitarios</h3>
+        En una vivienda o edificio de viviendas, es muy baja la probabilidad de que todos los artefactos
+        instalados, estén en funcionamiento simultáneamente. Un ejemplo de esto, aunque siendo simple es
+        clarificador, es el del baño completo, que posee cuatro artefactos instalados, ducha, inodoro, bidé y
+        lavatorio, de los cuales, - en la generalidad de los casos-, solamente uno estará en funcionamiento.
+        Es decir, habrá un artefacto en funcionamiento sobre cuatro instalados. Si ampliamos la instalación a
+        un departamento completo, puede existir la probabilidad de que además del funcionamiento de un
+        artefacto de baño, también se use la pileta de cocina o la pileta de lavar o bien ambas simultáneamente.
+        Si seguimos ampliando la instalación al edificio de departamentos, el problema de determinar cuáles y
+        cuantos artefactos y en que períodos de tiempo se usan simultáneamente, se torna complejo. Y como
+        consecuencia de ello, es también compleja la determinación de los caudales circulantes por la instalación.
+        Existirán edificios con destinos particulares, donde se tendrá certeza, en algunos casos total, sobre
+        el uso simultáneo de los artefactos y el tiempo de duración de este uso. Por ejemplo, edificios como
+        escuelas y cuarteles suponen un uso intensivo de las instalaciones en ciertos períodos de tiempo, por
+        lo cual el caudal instantáneo puede ser igual al caudal instalado. Existen varios métodos para considerar
+        la simultaneidad de los consumos que se pueden agrupar de la siguiente manera:
+        <ul>
+          <li>Métodos Probabilísticos</li>
+          <li>Métodos Empíricos</li>
+          <li>Métodos Semi-empíricos</li>
+        </ul>
+         
+El lector interesado puede ampliar conceptos sobre los métodos probabilísticos en la Referencia 7
+1
+Se deberá considerar el mayor caudal de los cuatros instaladosIng. Gustavo L. Lazarte-Ing. Viviana B. Sánchez 48 /63
+Cabe aclarar que, siendo las Normas OSN, normas de carácter empírico, no tratan analíticamente el problema de la
+simultaneidad en distribución de agua.
+No obstante, dada la complejidad del problema cuando se trata de alimentar muchos artefactos, quedará a responsabilidad
+del proyectista, la utilización de uno u otro método conjuntamente con su criterio profesional y experiencia. En este sentido,
+el Código Técnico de la Edificación de España (Documento Básico HS4, 4.21. Dimensionado de los tramos), prescribe
+que el establecimiento de los coeficientes de simultaneidad de cada tramo debe hacerse de acuerdo con un criterio
+adecuado, sin indicar uno en particular, por lo tanto, es el proyectista quien debe decidir sobre este tema. Por otro lado,
+“Crear un modelo exacto para predecir la demanda de una edificación es imposible y estudios finales que consideren el
+impacto de la conservación en la demanda del agua no están completos todavía”. (Código Internacional de Instalaciones
+Hidráulicas y Sanitarias, 2006).
+Entre los métodos semi-empíricos utilizados, particularmente en España y con antecedentes en normativa francesa, nos
+referiremos al Método del Factor de Simultaneidad, también llamado Método Racional y que ha sido recogido por el
+Reglamento de Instalaciones Sanitarias de la Ciudad Autónoma de Buenos Aires, para su aplicación en dimensionamiento
+de la instalación por métodos racionales.
         <h3>Cálculo de Caudales Simultaneos</h3>
         <h4>Método para Provisión por Servicio Directo (Normas OSN)</h4>
+        Para la determinación se distinguen dos casos:
+Viviendas o departamentos:
+Se adopta como Caudal Simultáneo (Qc), el correspondiente a una vez y medio el caudal de la canilla de servicio, esto es:
+𝑄𝑐 = 1.5𝑥0.13 𝑙𝑡𝑠
+𝑠
+0.20𝑙𝑡𝑠
+𝑠
+(39)
+Edificios de oficina, negocios y fábricas:
+𝑄𝑐 =
+𝑁 2
+0.13
+𝑙𝑡𝑠
+𝑠
+(40)
+Siendo N es el número de artefactos instalados. Conjuntos de artefactos de baños y toilette, se consideran un solo
+artefacto.
         <h4>Método del Factor de Simultaneidad K<sub>1</sub></h4>
+        Es un método semi-empírico, el Caudal Simultáneo se determina con la expresión (Díaz Dorado, 2005):
+𝑄𝑐 = 𝐾1𝑄𝑡 (41)
+Donde:
+Qc=Caudal Simultáneo en [lts/s]
+K1= Factor de Simultaneidad, adimensional
+Qt= Caudal Instalado en [lts/s]
+El factor de simultaneidad es menor que la unidad y se calcula con la siguiente expresión:
+𝐾1 =
+1
+√𝑛 − 1
+(42)
+Donde n es el número de artefactos aguas abajo del tramo de cañería considerado y que definen el caudal instalado.
+Se recomienda no considerar factores de simultaneidad menores a 0.20. Cabe acotar que este método requiere que los
+caudales unitarios asignados a los artefactos de consumo, numéricamente posean el mismo orden de magnitud.
+Para los tramos de la instalación que alimenten baños, se considerará un solo artefacto y será el de mayor caudal instalado.
+
         <h4>Método del Factor de Simultaneidad K<sub>2</sub></h4>
+        Según el número de unidades locativas o viviendas
+Cuando se distribuye agua a un conjunto de viviendas o unidades locativas, es de esperar que los hábitos de consumo no
+sean iguales en cada una de ellas.
+Un edificio de departamentos, por ejemplo, puede tener la misma dotación sanitaria en todas las unidades locativas, sin
+embargo los usos pueden no coincidir en el tiempo, pues son distintas las costumbres de las personas que las habitan. Una
+forma de tener en cuenta la no simultaneidad de los consumos de agua en este tipo de edificios, es mediante la introducción
+de otro coeficiente, que indicaremos como K2, que afecta a los caudales simultáneos determinados mediante el coeficiente
+K1
+El coeficiente K2 se aplica a los tramos troncales de la instalación que alimentan a las unidades locativas o viviendas y a
+los tramos de bajada en un edificio en altura.
+Se determina con la expresión:
+𝐾2 =
+19 + 𝑁
+10(𝑁 + 1)
+(43)
+Donde N es el número de unidades locativas o viviendas que alimenta el tramo que se está dimensionando.
+
+<h4>Método de la Norma UNE 149.201-08</h4> 
+Es un método para calcular caudales simultáneos en función del caudal instalado y del destino del edificio. A continuación
+transcribimos la formulación del caudal simultáneo, para dos destinos particulares:
+Edificios de Viviendas
+Para 𝑄𝑡 > 20 𝑙𝑡𝑠/𝑠 𝑄𝑐 = 1.7𝑥 (𝑄𝑡)0.21 − 0.7 𝑙𝑡𝑠/𝑠
+Para 𝑄𝑡 ≤ 20 𝑙𝑡𝑠/𝑠 dependiendo de los caudales instantáneos mínimos:
+Si todo 𝑄𝑚í𝑛 < 0.5 𝑙𝑡𝑠/𝑠 𝑄𝑐 = 0.682𝑥 (𝑄𝑇)0.45 − 0.14 𝑙𝑡𝑠/𝑠
+𝑄𝑡 ≤ 1 𝑙𝑡𝑠/𝑠 𝑄𝑐 = 𝑄𝑡 𝑁𝑜 𝑠𝑖𝑚𝑢𝑙𝑡𝑎𝑛𝑒𝑖𝑑𝑎𝑑
+Si algún 𝑄𝑚í𝑛 ≥ 0.5 𝑙𝑡𝑠/𝑠
+Qt=1.75 lts/s
+n=12
+K1=0.302
+Qc=0.53 lts/s
+2º P
+N=2
+K
+2=0.70
+Qc=0.74 lts/s
+N=4
+K
+2=0.46
+Qc=0.97 lts/s
+1º P
+Qt=1.75 lts/s
+n=12
+K1=0.302
+Qc=0.53 lts/s
+4º P
+Qt=1.75 lts/s
+n=12
+K1=0.302
+Qc=0.53 lts/s
+N=3
+K2=0.55
+Qc=0.87 lts/s
+3º P
+TANQUE DE RESERVA
+Qt=1.75 lts/s
+n=12
+K1=0.302
+Qc=0.53 lts/s
+N=1
+K2=1
+Qc=0.53 lts/sIng. Gustavo L. Lazarte-Ing. Viviana B. Sánchez 52 /63
+𝑄𝑡 > 1 𝑙𝑡𝑠/𝑠 𝑄𝑐 = 1.7𝑥 (𝑄𝑡)0.21 − 0.7 𝑙𝑡𝑠/𝑠
+Qmín en [lts/s], es el caudal asignado a cada artefacto de consumo.
+Edificios de Oficinas, Estaciones, Aeropuertos, Etc.
+Para 𝑄𝑡 > 20 𝑙𝑡𝑠/𝑠 𝑄𝑐 = 0.4𝑥 (𝑄𝑡)0.54 + 0.48 𝑙𝑡𝑠/𝑠
+Para 𝑄𝑡 ≤ 20 𝑙𝑡𝑠/𝑠 dependiendo de los caudales instantáneos mínimos:
+Si todo 𝑄𝑚í𝑛 < 0.5 𝑙𝑡𝑠/𝑠 𝑄𝑐 = 0.682𝑥 (𝑄𝑇)0.45 − 0.14 𝑙𝑡𝑠/𝑠
+𝑄𝑡 ≤ 1 𝑙𝑡𝑠/𝑠 𝑄𝑐 = 𝑄𝑡 𝑁𝑜 𝑠𝑖𝑚𝑢𝑙𝑡𝑎𝑛𝑒𝑖𝑑𝑎𝑑
+Si algún 𝑄𝑚í𝑛 ≥ 0.5 𝑙𝑡𝑠/𝑠
+𝑄𝑡 > 1 𝑙𝑡𝑠/𝑠 𝑄𝑐 = 1.7𝑥 (𝑄𝑡)0.21 − 0.7 𝑙𝑡𝑠/𝑠
+
+<h4>Método de la Norma Chilena NCH-2.485-00</h4>
+La Norma Chilena NCh2485.Of2000, establece el siguiente procedimiento para la determinación de caudales instantáneos:
+𝑄𝑐 = 1.7391𝑥𝑄𝑇0.6891
+Donde:
+QT= caudal instalado en [lts/min]
+Qc=caudal máximo probable en [lts/min]
+Para aplicación de la fórmula, la Norma prescribe que, para “los dos últimos artefactos de un tramo de ramal, el caudal
+máximo probable debe ser la suma de los caudales instalados de ambos”.
+Y el “caudal máximo probable de un tramo de ramal en que existen tres o más artefactos debe ser, como mínimo la suma
+de los dos de mayor consumo”.
+<h4>Caudales Simultáneos cuando la instalación posee Válvulas Automáticas para inodoros</h4>  
+Las válvulas automáticas para inodoros descargan un gran caudal instantáneamente, puesto que al no disponerse de
+depósito, el agua se almacena en la propia cañería que alimenta la válvula.
+Se usan particularmente en sanitarios de uso público, y es recomendable que sean alimentadas por una instalación
+independiente de otros artefactos, como son inodoros con depósito, lavatorios, mingitorios, etc., en razón de que el caudal
+de desacarga de la válvula automática puede perturbar el uso de otros artefactos. La alimentación independiente debe
+hacerse desde el colector o puente de empalme del tanque de reserva.
+Dado el corto tiempo de funcionamiento de la válvula, en el orden de los 30 segundos, es muy baja la probabilidad de uso
+simultaneo de varias válvulas, es por ello que las Normas OSN consideran una válvula en funcionamiento por cada cuatro
+válvulas instaladas. Este criterio ha sido adoptado en general, por los fabricantes nacionales de válvulas automáticas.
+Otro criterio aceptado es (Carnicer Royo, 1998):
+1 Válvula automática si la instalación tiene un número inferior a 3.Ing. Gustavo L. Lazarte-Ing. Viviana B. Sánchez 53 /63
+2 Válvulas automáticas si la instalación tiene un número entre 3 y 12
+3 Válvulas automáticas si la instalación tiene un número entre 12 y 24.
+4 Válvulas automáticas si la instalación tiene un número mayor a 24
+
+
 
       </>,
     },
